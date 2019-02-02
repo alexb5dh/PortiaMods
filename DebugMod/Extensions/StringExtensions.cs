24d3eb816397febba320cb667ef23ba29a6f9e4b
Oleksii Bespalov <alexb5dh@gmail.com>
2019-02-02 03:02:39 +0200

    DebugMod: first version

---
+++ DebugMod/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DebugMod.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool EqualsOrdinal(this string @this, string value) => string.Equals(@this, value, StringComparison.Ordinal);
+
+        public static string[] Split(this string @this, char separator, StringSplitOptions options) => @this.Split(new[] { separator }, options);
+
+        public static string[] Split(this string @this, StringSplitOptions options) => @this.Split((char[])null, options);
+    }
+}
