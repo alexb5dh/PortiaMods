246181520c59ebe9eaab182e0b1f56b112b58d00
Oleksii Bespalov <alexb5dh@gmail.com>
2019-02-18 11:10:33 +0200

    TweakIt: adjusted default settings

--- TweakIt/Settings.cs
+++ TweakIt/Settings.cs
@@ -4,13 +4,13 @@ namespace TweakIt
 {
     public class Settings: UnityModManager.ModSettings
     {
-        public bool DefaultCraftToMax { get; set; }
-
-        public bool RemoveCookingStun { get; set; }
-
         public bool SortChestsByName { get; set; } = true;
 
+        public bool DefaultCraftToMax { get; set; } = true;
+
         public bool DetailedNotifications { get; set; } = true;
+
+        public bool RemoveCookingStun { get; set; } = true;
 
         public override void Save(UnityModManager.ModEntry modEntry) => Save(this, modEntry);
     }
